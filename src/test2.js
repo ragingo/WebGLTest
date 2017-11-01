2861df36a4641cae6700c0ffb04482d6da8276cd
ragingo <save.the.world.crush@gmail.com>
2017-11-02 05:15:14 +0900

    typescriptへの移行の準備

---
+++ src/test2.js
@@ -0,0 +1,14 @@
+var Graphics2 = (function () {
+    function Graphics2(gl) {
+        this.gl = gl;
+    }
+    Graphics2.prototype.init = function () {
+    };
+
+    Graphics2.prototype.prepare = function () {
+    };
+
+    Graphics2.prototype.render = function () {
+    };
+    return Graphics2;
+})();
---
+++ src/test2.ts
@@ -0,0 +1,20 @@
+class Graphics2 {
+
+	gl: WebGLRenderingContext;
+
+	constructor(gl: WebGLRenderingContext) {
+		this.gl = gl;
+	}
+
+	init(): boolean {
+		return true;
+	}
+
+	prepare(): boolean{
+		return true;
+	}
+
+	render(): void {
+	}
+
+}
