05150eb6bcdecaae20633b9680c631931248d042
ragingo <save.the.world.crush@gmail.com>
2017-11-07 02:15:57 +0900

    テクスチャ切り出し修正

--- src/glsl/texture_slice_fs.glsl
+++ src/glsl/texture_slice_fs.glsl
@@ -4,18 +4,34 @@ uniform sampler2D uSampler;
 uniform vec2      textureSize;
 varying vec2      vTextureCoord;
 
+bool includes(vec2 uv, float left, float top, float right, float bottom)
+{
+	float l = left / 512.0;
+	float t = top / 512.0;
+	float r = right / 512.0;
+	float b = bottom / 512.0;
+	if (uv.x >= l && uv.x <= r &&
+		uv.y >= t && uv.y <= b) {
+		return true;
+	}
+	return false;
+}
 
 void main() {
 
 	vec2 uv = vTextureCoord;
 
-	if (uv.x >= (8.0/512.0) && uv.x <= (15.0/512.0) &&
-		uv.y >= (8.0/512.0) && uv.y <= (15.0/512.0)) {
-
-		vec4 color = texture2D(uSampler, uv * vec2(1.0, 1.0));
-		gl_FragColor = color;
+	// "A" を切り取り
+	if (includes(uv, 8.0, 8.0, 16.0, 16.0)) {
+		gl_FragColor = texture2D(uSampler, uv);
+	}
+	// "C" を切り取り
+	// TODO: A と C の間を埋める(B の位置に C を移動する)
+	else if (includes(uv, 24.0, 8.0, 32.0, 16.0)) {
+		gl_FragColor = texture2D(uSampler, uv);
 	}
 	else {
 		gl_FragColor = vec4(0.0);
 	}
+
 }
