c460abf25b5dbdfd699789b08f69177c6919aeb8
ragingo <save.the.world.crush@gmail.com>
2017-11-21 22:59:26 +0900

    utilsのファイル分割

--- src/js/main.js
+++ src/js/main.js
@@ -629,47 +629,17 @@ class Sprite {
         this.m_MainTexture = texture;
     }
 }
-class HttpUtil {
-    static async get(url, responseType) {
-        return new Promise(resolve => {
-            let xhr = new XMLHttpRequest();
-            xhr.addEventListener("loadend", function () {
-                if (xhr.readyState != 4) {
-                    console.log("not ready.");
-                    return;
-                }
-                if (xhr.status != 200) {
-                    console.log("http response status error.");
-                    return;
-                }
-                resolve(xhr);
-                console.log("request completed.(" + url + ")");
-            });
-            if (responseType) {
-                xhr.responseType = responseType;
-            }
-            xhr.open("GET", url);
-            xhr.send();
-            console.log("start http request. (" + url + ")");
-        });
+class ArrayUtil {
+    static toHTMLElements(array) {
+        let result = [];
+        for (let i = 0; i < array.length; i++) {
+            let item = array[i];
+            result.push(item);
+        }
+        return result;
     }
-    static async getText(url) {
-        let xhr = await HttpUtil.get(url);
-        return new Promise(resolve => {
-            return resolve(xhr.responseText);
-        });
-    }
-    static async getRaw(url) {
-        let xhr = await HttpUtil.get(url);
-        return new Promise(resolve => {
-            return resolve(xhr.response);
-        });
-    }
-    static async getBinary(url) {
-        let xhr = await HttpUtil.get(url, "arraybuffer");
-        return new Promise(resolve => {
-            return resolve(xhr.response);
-        });
+    static pushAll(src, dst) {
+        src.forEach(x => dst.push(x));
     }
 }
 class DomUtil {
@@ -714,17 +684,47 @@ class DomUtil {
         return null;
     }
 }
-class ArrayUtil {
-    static toHTMLElements(array) {
-        let result = [];
-        for (let i = 0; i < array.length; i++) {
-            let item = array[i];
-            result.push(item);
-        }
-        return result;
+class HttpUtil {
+    static async get(url, responseType) {
+        return new Promise(resolve => {
+            let xhr = new XMLHttpRequest();
+            xhr.addEventListener("loadend", function () {
+                if (xhr.readyState != 4) {
+                    console.log("not ready.");
+                    return;
+                }
+                if (xhr.status != 200) {
+                    console.log("http response status error.");
+                    return;
+                }
+                resolve(xhr);
+                console.log("request completed.(" + url + ")");
+            });
+            if (responseType) {
+                xhr.responseType = responseType;
+            }
+            xhr.open("GET", url);
+            xhr.send();
+            console.log("start http request. (" + url + ")");
+        });
     }
-    static pushAll(src, dst) {
-        src.forEach(x => dst.push(x));
+    static async getText(url) {
+        let xhr = await HttpUtil.get(url);
+        return new Promise(resolve => {
+            return resolve(xhr.responseText);
+        });
+    }
+    static async getRaw(url) {
+        let xhr = await HttpUtil.get(url);
+        return new Promise(resolve => {
+            return resolve(xhr.response);
+        });
+    }
+    static async getBinary(url) {
+        let xhr = await HttpUtil.get(url, "arraybuffer");
+        return new Promise(resolve => {
+            return resolve(xhr.response);
+        });
     }
 }
 class ViewBase {
---
+++ src/ts/util/HttpUtil.ts
@@ -0,0 +1,49 @@
+
+class HttpUtil {
+
+	public static async get(url: string, responseType?: string): Promise<XMLHttpRequest> {
+		return new Promise<XMLHttpRequest>(resolve => {
+			let xhr = new XMLHttpRequest();
+			xhr.addEventListener("loadend", function () {
+				if (xhr.readyState != 4) {
+					console.log("not ready.");
+					return;
+				}
+				if (xhr.status != 200) {
+					console.log("http response status error.");
+					return;
+				}
+				resolve(xhr);
+				console.log("request completed.(" + url + ")");
+			});
+			if (responseType) {
+				xhr.responseType = responseType as XMLHttpRequestResponseType;
+			}
+			xhr.open("GET", url);
+			xhr.send();
+			console.log("start http request. (" + url + ")");
+		});
+	}
+
+	public static async getText(url: string): Promise<string> {
+		let xhr = await HttpUtil.get(url);
+		return new Promise<string>(resolve => {
+			return resolve(xhr.responseText);
+		});
+	}
+
+	public static async getRaw(url: string): Promise<string> {
+		let xhr = await HttpUtil.get(url);
+		return new Promise<string>(resolve => {
+			return resolve(xhr.response);
+		});
+	}
+
+	public static async getBinary(url: string): Promise<ArrayBuffer> {
+		let xhr = await HttpUtil.get(url, "arraybuffer");
+		return new Promise<ArrayBuffer>(resolve => {
+			return resolve(xhr.response as ArrayBuffer);
+		});
+	}
+
+}
