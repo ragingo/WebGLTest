56511ab9090d0f1c5ef3e80d295dac1e86466803
ragingo <save.the.world.crush@gmail.com>
2020-10-25 23:00:39 +0900

    コード整理

--- src/ts/views/MainView.ts
+++ src/ts/views/MainView.ts
@@ -1,19 +1,37 @@
+import { Slider } from "./Slider";
+
 const getBySelector = <T extends HTMLElement>(selector: string): T => {
   return document.querySelector(selector) as T;
 }
+
+const effects = [
+  { id: 0, name: '通常' },
+  { id: 1, name: 'グレースケール' },
+  { id: 2, name: '2値化' },
+  { id: 3, name: '二次微分 laplacian 4方向' },
+  { id: 4, name: '二次微分 laplacian 8方向' },
+  { id: 5, name: '一次微分 Roberts' },
+  { id: 6, name: 'Prewitt' },
+  { id: 7, name: '階調反転' },
+  { id: 8, name: '色深度 8ビット' },
+  { id: 9, name: '色深度 15ビット' },
+  { id: 10, name: '色深度 16ビット' },
+  { id: 11, name: '色深度 24ビット' },
+  { id: 12, name: '色深度 32ビット' },
+  { id: 13, name: '円' },
+  { id: 14, name: '球' },
+  { id: 15, name: '正弦波' },
+  { id: 16, name: '鮮やか' },
+];
 
 export class MainView {
   private reset: HTMLButtonElement;
   private fpsLabel: HTMLParagraphElement;
   private effectSelector: HTMLSelectElement;
-  private color: HTMLInputElement[] = [];
-  private colorLabels: HTMLLabelElement[] = [];
-  private rotation: HTMLInputElement[] = [];
-  private rotationLabels: HTMLLabelElement[] = [];
-  private scale: HTMLInputElement[] = [];
-  private scaleLabels: HTMLLabelElement[] = [];
-  private vivid: HTMLInputElement[] = [];
-  private vividLabels: HTMLLabelElement[] = [];
+  private color: Slider[] = [];
+  private rotation: Slider[] = [];
+  private scale: Slider[] = [];
+  private vivid: Slider[] = [];
 
   public canvas: HTMLCanvasElement;
 
@@ -21,43 +39,39 @@ export class MainView {
     this.fpsLabel.innerText = value;
   }
 
-  public getColorValue() {
-    const color = Array.from(this.color.map((x) => x.valueAsNumber));
-    return {
-      r: color[0],
-      g: color[1],
-      b: color[2],
-      a: color[3],
-    };
-  }
-
   public getEffectTypeValue() {
     return parseInt(this.effectSelector.options[this.effectSelector.selectedIndex].value);
   }
 
   public getRotationValue() {
-    const rotation = Array.from(this.rotation.map((x) => x.valueAsNumber));
     return {
-      x: rotation[0],
-      y: rotation[1],
-      z: rotation[2],
+      x: this.rotation[0].value,
+      y: this.rotation[1].value,
+      z: this.rotation[2].value,
     };
   }
 
   public getScaleValue() {
-    const scale = Array.from(this.scale.map((x) => x.valueAsNumber));
     return {
-      x: scale[0],
-      y: scale[1],
-      z: scale[2],
+      x: this.scale[0].value,
+      y: this.scale[1].value,
+      z: this.scale[2].value,
+    };
+  }
+
+  public getColorValue() {
+    return {
+      r: this.color[0].value,
+      g: this.color[1].value,
+      b: this.color[2].value,
+      a: this.color[3].value,
     };
   }
 
   public getVividValue() {
-    const vivid = Array.from(this.vivid.map((x) => x.valueAsNumber));
     return {
-      k1: vivid[0],
-      k2: vivid[1],
+      k1: this.vivid[0].value,
+      k2: this.vivid[1].value,
     };
   }
 
@@ -69,25 +83,6 @@ export class MainView {
     this.canvas = getBySelector('.app__canvas');
 
     this.effectSelector = getBySelector('.app__effect-selector');
-    const effects = [
-      { id: 0, name: '通常' },
-      { id: 1, name: 'グレースケール' },
-      { id: 2, name: '2値化' },
-      { id: 3, name: '二次微分 laplacian 4方向' },
-      { id: 4, name: '二次微分 laplacian 8方向' },
-      { id: 5, name: '一次微分 Roberts' },
-      { id: 6, name: 'Prewitt' },
-      { id: 7, name: '階調反転' },
-      { id: 8, name: '色深度 8ビット' },
-      { id: 9, name: '色深度 15ビット' },
-      { id: 10, name: '色深度 16ビット' },
-      { id: 11, name: '色深度 24ビット' },
-      { id: 12, name: '色深度 32ビット' },
-      { id: 13, name: '円' },
-      { id: 14, name: '球' },
-      { id: 15, name: '正弦波' },
-      { id: 16, name: '鮮やか' },
-    ];
     effects.forEach((x) => {
       const opt = document.createElement('option');
       opt.value = String(x.id);
@@ -99,75 +94,37 @@ export class MainView {
     };
 
     this.rotation = [
-      getBySelector('.rotation--x > .rotation__slider'),
-      getBySelector('.rotation--y > .rotation__slider'),
-      getBySelector('.rotation--z > .rotation__slider'),
+      new Slider('.rotation--x > .rotation__slider', '.rotation--x > .rotation__label'),
+      new Slider('.rotation--y > .rotation__slider', '.rotation--y > .rotation__label'),
+      new Slider('.rotation--z > .rotation__slider', '.rotation--z > .rotation__label'),
     ];
-    this.rotationLabels = [
-      getBySelector('.rotation--x > .rotation__label'),
-      getBySelector('.rotation--y > .rotation__label'),
-      getBySelector('.rotation--z > .rotation__label'),
-    ];
-    this.rotation.forEach((x, i) => {
-      x.oninput = () => this.onRotationChanged(x, i);
-    });
 
     this.scale = [
-      getBySelector('.scale--x > .scale__slider'),
-      getBySelector('.scale--y > .scale__slider'),
-      getBySelector('.scale--z > .scale__slider'),
+      new Slider('.scale--x > .scale__slider', '.scale--x > .scale__label'),
+      new Slider('.scale--y > .scale__slider', '.scale--y > .scale__label'),
+      new Slider('.scale--z > .scale__slider', '.scale--z > .scale__label'),
     ];
-    this.scaleLabels = [
-      getBySelector('.scale--x > .scale__label'),
-      getBySelector('.scale--y > .scale__label'),
-      getBySelector('.scale--z > .scale__label'),
-    ];
-    this.scale.forEach((x, i) => {
-      x.oninput = () => this.onScaleChanged(x, i);
-    });
 
     this.color = [
-      getBySelector('.color--r > .color__slider'),
-      getBySelector('.color--g > .color__slider'),
-      getBySelector('.color--b > .color__slider'),
-      getBySelector('.color--a > .color__slider'),
+      new Slider('.color--r > .color__slider', '.color--r > .color__label'),
+      new Slider('.color--g > .color__slider', '.color--g > .color__label'),
+      new Slider('.color--b > .color__slider', '.color--b > .color__label'),
+      new Slider('.color--a > .color__slider', '.color--a > .color__label'),
     ];
-    this.colorLabels = [
-      getBySelector('.color--r > .color__label'),
-      getBySelector('.color--g > .color__label'),
-      getBySelector('.color--b > .color__label'),
-      getBySelector('.color--a > .color__label'),
-    ];
-    this.color.forEach((x, i) => {
-      x.oninput = () => this.onColorChanged(x, i);
-    });
-
 
     this.vivid = [
-      getBySelector('.vivid--k1 > .vivid__slider'),
-      getBySelector('.vivid--k2 > .vivid__slider'),
+      new Slider('.vivid--k1 > .vivid__slider', '.vivid--k1 > .vivid__label'),
+      new Slider('.vivid--k2 > .vivid__slider', '.vivid--k2 > .vivid__label'),
     ];
-    this.vividLabels = [
-      getBySelector('.vivid--k1 > .vivid__label'),
-      getBySelector('.vivid--k2 > .vivid__label'),
-    ];
-    this.vivid.forEach((x, i) => {
-      x.oninput = () => this.onVividChanged(x, i);
-    });
   }
 
   public resetValues() {
-    this.rotation.forEach((x) => (x.value = x.defaultValue));
-    this.scale.forEach((x) => (x.value = x.defaultValue));
-    this.color.forEach((x) => (x.value = x.defaultValue));
     this.effectSelector.value = '0';
-    this.vivid.forEach((x) => (x.value = x.defaultValue));
-
-    this.rotation.forEach((x) => x.dispatchEvent(new Event('input')));
-    this.scale.forEach((x) => x.dispatchEvent(new Event('input')));
-    this.color.forEach((x) => x.dispatchEvent(new Event('input')));
     this.effectSelector.dispatchEvent(new Event('change'));
-    this.vivid.forEach((x) => x.dispatchEvent(new Event('input')));
+    this.rotation.forEach((x) => x.reset());
+    this.scale.forEach((x) => x.reset());
+    this.color.forEach((x) => x.reset());
+    this.vivid.forEach((x) => x.reset());
   }
 
   private onResetClick = () => {
@@ -181,21 +138,5 @@ export class MainView {
     } else {
       elem.style.display = 'none';
     }
-  }
-
-  private onColorChanged(sender: HTMLInputElement, index: number) {
-    this.colorLabels[index].innerText = sender.value;
-  }
-
-  private onRotationChanged(sender: HTMLInputElement, index: number) {
-    this.rotationLabels[index].innerText = sender.value;
-  }
-
-  private onScaleChanged(sender: HTMLInputElement, index: number) {
-    this.scaleLabels[index].innerText = sender.value;
-  }
-
-  private onVividChanged(sender: HTMLInputElement, index: number) {
-    this.vividLabels[index].innerText = sender.value;
   }
 }
