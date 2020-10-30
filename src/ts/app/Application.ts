e9bc40e3800f13fe468f5b35bdc6dfd73a5bcd04
ragingo <save.the.world.crush@gmail.com>
2020-10-31 07:37:07 +0900

    コード整理

--- src/ts/app/Application.ts
+++ src/ts/app/Application.ts
@@ -2,38 +2,17 @@ import { Graphics } from "../gfx/Graphics";
 import { MainScene } from "./MainScene";
 
 export class Application {
-
   public static main() {
     const gfx = new Graphics();
     gfx.pushScene(new MainScene());
     gfx.init(512, 512);
     gfx.prepare();
 
-    // let frameCount = 0;
-    let now = 0.0;
-    let last = 0.0;
-    let elapsed = 0.0;
-
-    const frameRequestCallback = (time: number) => {
-      now = time;
-      elapsed += now - last;
-      last = now;
-
-      // frameCount++;
-
-      if (elapsed >= 1000) {
-        // Application.s_AppFrames.forEach((f) => {
-        //   f.onFpsUpdate(frameCount);
-        // });
-        // frameCount = 0;
-        elapsed -= 1000.0;
-      }
-
+    const callback = () => {
       gfx.render();
-
-      window.requestAnimationFrame(frameRequestCallback);
+      window.requestAnimationFrame(callback);
     };
 
-    window.requestAnimationFrame(frameRequestCallback);
+    window.requestAnimationFrame(callback);
   }
 }
